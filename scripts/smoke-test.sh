28d8cf3a8a5288794e2238088a0f392e33ded465
Ashish Dwivedi <theashishdwivedi525@gmail.com>
2026-09-10 13:54:18 +0000

    bug fixed

--- scripts/smoke-test.sh
+++ scripts/smoke-test.sh
@@ -22,9 +22,9 @@ set -uo pipefail
 RETRIES="${SMOKE_TEST_RETRIES:-5}"
 DELAY="${SMOKE_TEST_RETRY_DELAY:-5}"
 
-API_HEALTH_URL="${API_HEALTH_URL:-https://api.nityasamagri.in/health}"
+API_HEALTH_URL="${API_HEALTH_URL:-https://api.adminns.in/health}"
 WEB_URL="${WEB_URL:-https://nityasamagri.in}"
-ADMIN_URL="${ADMIN_URL:-https://admin.nityasamagri.in}"
+ADMIN_URL="${ADMIN_URL:-https://adminns.in}"
 
 FAILED=0
 
